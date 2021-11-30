4bf02c2de508fcaa37621fb8cfaa6dd6614bcccf
Busuyi Adebayo <your.BusuyiOAdebayo@gmail.com>
2021-11-30 12:41:02 -0600

    My Homework3 recommitted

--- Homework3/Homework3.js
+++ Homework3/Homework3.js
@@ -5,8 +5,16 @@ const pizzaToppings = ["pepperoni", "mushrooms", "onions", "black olives", "bana
 
 // Create pizzaToppings String
 let pizzaToppingsString = "";
-for (let element of pizzaToppings){
+/*for (let element of pizzaToppings) {
 pizzaToppingsString += element + ", ";
+}*/
+for (let i = 0; i < pizzaToppings.length; i++) {
+  if (i < pizzaToppings.length-1) {
+    pizzaToppingsString += pizzaToppings[i] + ", ";
+  }
+  else {
+    pizzaToppingsString += pizzaToppings[i];
+  }
 }
 
 // Create greetCustomer Function
@@ -14,12 +22,14 @@ function greetCustomer() {
 console.log (`Welcome to Busuyi's Palace Pizza, our toppings today are ${pizzaToppingsString}. When you are ready, kindly place your order, THANK YOU!`);
 }
 
-//Declare toppings Global Variable
-let toppings;
-
 // Create getPizzaOrder Function
 function getPizzaOrder(size, crust, ...toppings) {
-console.log (`One ${size} ${crust} pizza with toppings: ${toppings} coming up!`);
+  if (toppings == []) {
+    console.log(`One ${size} ${crust} pizza coming up!`);
+  }
+  else {
+    console.log (`One ${size} ${crust} pizza with toppings: ${toppings} coming up!`);
+  }
 return [size, crust, toppings];
 }
 
@@ -31,12 +41,21 @@ return {size:size, crust:crust, toppings:toppings};
 
 // Create servePizza Function
 function servePizza({size, crust, toppings}) {
-console.log (`Order up! Here's your ${size} ${crust} pizza with toppings: ${toppings}. Enjoy!`);
+  if (toppings == []) {
+    console.log (`Order up! Here's your ${size} ${crust} pizza. Enjoy!`);
+  }
+  else {
+    console.log (`Order up! Here's your ${size} ${crust} pizza with toppings: ${toppings}. Enjoy!`);
+  }
 return {size:size, crust:crust, toppings:toppings};
 }
 
 // Function Calling
 greetCustomer();
-const returnvalueofgetPizzaOrder = getPizzaOrder("large", "thick", ["pepperoni", " mushrooms", " onions"]);
-const returnvalueofpreparePizza = preparePizza(returnvalueofgetPizzaOrder);
+toppings = ["pepperoni", " mushrooms", " onions"];
+let returnvalueofgetPizzaOrder = getPizzaOrder("large", "thick", toppings);
+let returnvalueofpreparePizza = preparePizza(returnvalueofgetPizzaOrder);
 servePizza(returnvalueofpreparePizza);
+
+// Console.log the Final Pizza Object
+console.log(returnvalueofpreparePizza);
